0f7100d1646046b0882dfb9e88ada0b71cf6563a
ndry0611 <andryss0611@gmail.com>
2023-10-21 13:47:20 +0700

    add middleware

---
+++ pkg/helpers/jwt.go
@@ -0,0 +1,52 @@
+package helpers
+
+import (
+	"errors"
+	"final_project_3/infrastructure/config"
+	"strings"
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func GenerateToken(id int, email string) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"id":    id,
+		"email": email,
+	})
+	tokenString, err := token.SignedString([]byte(config.GetAppConfig().JWTSecretKey))
+	if err != nil {
+		return "", err
+	}
+	return tokenString, nil
+}
+
+func VerifyToken(ctx *gin.Context) (interface{}, error) {
+	errResp := errors.New("invalid authorization token")
+	getToken := ctx.Request.Header.Get("Authorization")
+	bearer := strings.HasPrefix(getToken, "Bearer")
+
+	if !bearer{
+		return nil, errResp
+	}
+
+	tokenString := strings.Split(getToken, " ")[1]
+
+	token, err := jwt.Parse(tokenString, func(t *jwt.Token) (interface{}, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", t.Header["alg"])
+		}
+
+		return []byte(config.GetAppConfig().JWTSecretKey), nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if _, ok := token.Claims.(jwt.MapClaims); !ok && !token.Valid {
+		return nil, err
+	}
+	return token.Claims.(jwt.MapClaims), nil
+}
---
+++ pkg/middlewares/authentication.go
@@ -0,0 +1,28 @@
+package middlewares
+
+import (
+	"final_project_3/helpers"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Authentication() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		tokenClaims, err := helpers.VerifyToken(c)
+
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"message": "user is unauthorized",
+				"error":   err.Error(),
+			})
+			return
+		}
+		c.Set("user", tokenClaims)
+		c.Next()
+
+		return
+
+	}
+}
